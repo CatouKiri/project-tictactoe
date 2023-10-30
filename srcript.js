724b95606c109d636e1d92d653b4e289995a1506
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-10-30 23:05:45 +0800

    update .html, .css, & .js

---
+++ srcript.js
@@ -0,0 +1,22 @@
+const Gameboard = (function () {
+    const domGameboard =
+    [1,2,3,
+     4,5,6,
+     7,8,9];
+    const winningPattern =
+    [{1,2,3},
+     {4,5,6},
+     {7,8,9},
+     {1,4,7},
+     {2,5,8},
+     {3,6,9},
+     {1,5,9},
+     {3,5,7}];
+})
+
+const Player = (function() {
+})
+
+const displayController = (function() {
+
+})
